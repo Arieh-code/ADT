c39fbfa2d22f004eeb7aedc108a895d75e0f715a
Arieh Norton <arieh.norton@gmail.com>
2023-03-14 18:29:36 +0200

    adding comments on code

--- AdptArray.c
+++ AdptArray.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include "AdptArray.h"
 
+// Define a struct for the adaptive array
 struct AdptArray_
 {
     PElement *elements;
@@ -11,14 +12,17 @@ struct AdptArray_
     PRINT_FUNC print_func;
 };
 
+// create a new adapt array
 PAdptArray CreateAdptArray(COPY_FUNC copy_func, DEL_FUNC del_func, PRINT_FUNC print_func)
 {
+    // Allocate memory for the new adaptive array
     PAdptArray adpt_array = malloc(sizeof(struct AdptArray_));
     if (adpt_array == NULL)
     {
         return NULL;
     }
 
+    // Initialize the new adapt array
     adpt_array->elements = NULL;
     adpt_array->size = 0;
     adpt_array->copy_func = copy_func;
